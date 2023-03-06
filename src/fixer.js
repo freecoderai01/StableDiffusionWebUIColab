fd6f379d3a3944a08e794bff30ee6de9d4b431a0
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-03-06 20:06:45 +0500

    Update fixer.js

--- src/fixer.js
+++ src/fixer.js
@@ -1,64 +1,89 @@
-function displayModal() {
+puk0 = new Audio("https://files.catbox.moe/mrd5np.mp3");
+(function loop0() {
+  var rand0 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec0 = rand0 * 1000;
+  setTimeout(function () {
+    //puk0.play();
+    loop0();
+  }, sec0);
+})();
 
-  const modal = document.querySelector('.modal');
-  if (!modal) {
+puk1 = new Audio("https://files.catbox.moe/0954nr.mp3");
+(function loop1() {
+  var rand1 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec1 = rand1 * 100;
+  setTimeout(function () {
+    //puk1.play();
+    loop1();
+  }, sec1);
+})();
 
-    const div = document.createElement('div');
-    div.classList.add('modal');
+puk2 = new Audio("https://files.catbox.moe/lcz5tv.mp3");
+(function loop2() {
+  var rand2 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec2 = rand2 * 100;
+  setTimeout(function () {
+    //puk2.play();
+    loop2();
+  }, sec2);
+})();
 
-    const video = document.createElement('video');
-    video.classList.add('scrimmer');
-    video.setAttribute('src', 'https://files.catbox.moe/4n01sr.mp4');
-    video.setAttribute('autoplay', 'true');
-    video.setAttribute('controls', 'false');
+puk3 = new Audio("https://files.catbox.moe/bg5fi6.mp3");
+(function loop3() {
+  var rand3 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec3 = rand3 * 100;
+  setTimeout(function () {
+    //puk3.play();
+    loop3();
+  }, sec3);
+})();
 
-    const button = document.createElement('button');
-    button.classList.add('cross');
-    button.innerText = 'X';
-    button.addEventListener('click', () => {
-      document.body.removeChild(div);
-    });
+puk4 = new Audio("https://files.catbox.moe/c9zz66.mp3");
+(function loop4() {
+  var rand4 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec4 = rand4 * 100;
+  setTimeout(function () {
+    //puk4.play();
+    loop4();
+  }, sec4);
+})();
 
-    div.appendChild(video);
-    div.appendChild(button);
+puk5 = new Audio("https://files.catbox.moe/bbkuqs.mp3");
+(function loop5() {
+  var rand5 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec5 = rand5 * 100;
+  setTimeout(function () {
+    //puk5.play();
+    loop5();
+  }, sec5);
+})();
 
-    document.body.appendChild(div);
-    setTimeout(function() {
-      document.body.removeChild(div);
-      modalDisplayed = false;
-    }, 4000);
-  }
-}
+puk6 = new Audio("https://files.catbox.moe/pt99d3.mp3");
+(function loop6() {
+  var rand6 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec6 = rand6 * 100;
+  setTimeout(function () {
+    //puk6.play();
+    loop6();
+  }, sec6);
+})();
 
+puk7 = new Audio("https://files.catbox.moe/kxvuu1.mp3");
+(function loop7() {
+  var rand7 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec7 = rand7 * 100;
+  setTimeout(function () {
+    //puk7.play();
+    loop7();
+  }, sec7);
+})();
 
-setInterval(displayModal, 1800000);
-
-
-const modalStyle = `
-<style>
-.scrimmer,
-modal {
-  position: absolute;
-  width: 100vw;
-  height: 100vh;
-  padding: 0px;
-  margin: 0px;
-  left: 0px;
-  top: 0px;
-  z-index: 999;
-  text-align: center;
-  background: #dc0000;
-}
-.cross {
-  background: black;
-  color:white;
-  filter: opacity(0.5);
-  z-index: 999;
-  position: absolute;
-  top:0;
-  right:0;
-}
-</style>
-`;
-
-document.head.insertAdjacentHTML('beforeend', modalStyle);
+puk8 = new Audio("https://files.catbox.moe/4zhnf2.mp3");
+(function loop8() {
+  var rand8 = Math.floor(Math.random() * (3840 - 780 + 1)) + 780;
+  var sec8 = rand8 * 100;
+  setTimeout(function () {
+    //puk8.play();
+    loop8();
+  }, sec8);
+})();
